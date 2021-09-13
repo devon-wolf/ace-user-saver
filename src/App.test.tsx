db044613a23592edba9ce3c1e4c401d3002ec00b
devon-wolf <devon.wolfkiel@gmail.com>
2021-09-13 15:34:34 -0700

    Write initial failing test for locating search bar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import UserSaver from './pages/UserSaver';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+it('renders a search bar', () => {
+    render(<UserSaver />);
+    const searchBar = screen.getByLabelText('user search');
+    expect(searchBar).toBeInTheDocument();
 });
---
+++ src/pages/UserSaver.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSaver = (): JSX.Element => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserSaver;
